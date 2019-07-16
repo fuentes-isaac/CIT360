586c35a37cf85bcf0bb5aae162db6c909bc4124d
Isaac Fuentes Rojo <fue15003@byui.edu>
2019-07-16 00:01:57 -0500

    Create Mexico.java
    display de info about Mexico

---
+++ controller/Mexico.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaController;
+
+/**
+ *
+ * @author isaacfuentes
+ */
+public class Mexico {
+    public void bio() {
+		System.out.println("Mexico is a place located in Nort America, just below the United States.");
+	}
+}
