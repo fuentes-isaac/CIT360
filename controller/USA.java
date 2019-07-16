947621d0e7a48bb0e24b7e7abf677f0cc2f242dd
Isaac Fuentes Rojo <fue15003@byui.edu>
2019-07-16 00:02:47 -0500

    Create USA.java
    display USA information

---
+++ controller/USA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaController;
+
+/**
+ *
+ * @author isaacfuentes
+ */
+public class USA {
+    public void bio() {
+		System.out.println("USA is right between Mexico and Canada");
+	}
+}
